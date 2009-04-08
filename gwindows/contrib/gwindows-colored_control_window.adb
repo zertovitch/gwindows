d095f04f74096e47745c725626e9cb679ab08b11
avsaway <avsaway@2f326fbb-0abf-4209-8265-e07f4167bd40>
2009-04-08 18:51:05 +0000

    Added more control types and functions on these types.

--- gwindows/contrib/gwindows-colored_control_window.adb
+++ gwindows/contrib/gwindows-colored_control_window.adb
@@ -1,13 +1,10 @@
-with GWindows.Base;              use GWindows.Base;
 with GWindows.Drawing;           use GWindows.Drawing;
-with GWindows.Types;             use GWindows.Types;
-with GWindows.Windows;           use GWindows.Windows;
 
 package body GWindows.Colored_Control_Window is
 
    WM_ERASEBKGND        : constant := 20;
    WM_CTLCOLOREDIT      : constant := 307;
---     WM_CTLCOLORBTN       : constant := 309;
+   WM_CTLCOLORBTN       : constant := 309;
    WM_CTLCOLORSCROLLBAR : constant := 311;
    WM_CTLCOLORSTATIC    : constant := 312;
 
@@ -67,17 +64,63 @@ package body GWindows.Colored_Control_Window is
            Control.all in Colored_Label_Type'Class then
             Handle (Canvas, GWindows.Types.Handle (wParam));
             Background_Color (Canvas, Colored_Label_Type (Control.all).Color);
+            Text_Color (Canvas, Colored_Label_Type (Control.all).Text);
             Handle (Canvas, 0);
             Return_Value := Handle (Colored_Label_Type (Control.all).Brush);
+         elsif (message = WM_CTLCOLORSTATIC or
+                message = WM_CTLCOLOREDIT) and then
+           Control.all in Colored_Edit_Box_Type'Class then
+            Handle (Canvas, GWindows.Types.Handle (wParam));
+            Background_Color
+               (Canvas, Colored_Edit_Box_Type (Control.all).Color);
+            Text_Color (Canvas, Colored_Edit_Box_Type (Control.all).Text);
+            Handle (Canvas, 0);
+            Return_Value :=
+              Handle (Colored_Edit_Box_Type (Control.all).Brush);
          elsif (message = WM_CTLCOLORSTATIC or
                 message = WM_CTLCOLOREDIT) and then
            Control.all in Colored_Multi_Line_Edit_Box_Type'Class then
             Handle (Canvas, GWindows.Types.Handle (wParam));
             Background_Color
               (Canvas, Colored_Multi_Line_Edit_Box_Type (Control.all).Color);
+            Text_Color
+              (Canvas, Colored_Multi_Line_Edit_Box_Type (Control.all).Text);
             Handle (Canvas, 0);
             Return_Value :=
               Handle (Colored_Multi_Line_Edit_Box_Type (Control.all).Brush);
+         elsif (message = WM_CTLCOLORSTATIC or
+                message = WM_CTLCOLOREDIT) and then
+           Control.all in Colored_Combo_Box_Type'Class then
+            Handle (Canvas, GWindows.Types.Handle (wParam));
+            Background_Color
+              (Canvas, Colored_Combo_Box_Type (Control.all).Color);
+            Text_Color (Canvas, Colored_Combo_Box_Type (Control.all).Text);
+            Handle (Canvas, 0);
+            Return_Value :=
+              Handle (Colored_Combo_Box_Type (Control.all).Brush);
+         elsif (message = WM_CTLCOLORSTATIC or
+                message = WM_CTLCOLOREDIT) and then
+           Control.all in Colored_Drop_Down_Combo_Box_Type'Class then
+            Handle (Canvas, GWindows.Types.Handle (wParam));
+            Background_Color
+              (Canvas, Colored_Drop_Down_Combo_Box_Type (Control.all).Color);
+            Text_Color
+              (Canvas, Colored_Drop_Down_Combo_Box_Type (Control.all).Text);
+            Handle (Canvas, 0);
+            Return_Value :=
+              Handle (Colored_Drop_Down_Combo_Box_Type (Control.all).Brush);
+         elsif (message = WM_CTLCOLORSTATIC or
+                message = WM_CTLCOLOREDIT or
+                message = WM_CTLCOLORBTN) and then
+               Control.all in Colored_Check_Box_Type'Class then
+            Handle (Canvas, GWindows.Types.Handle (wParam));
+            Background_Color
+              (Canvas, Colored_Check_Box_Type (Control.all).Color);
+            Text_Color
+              (Canvas, Colored_Check_Box_Type (Control.all).Color);
+            Handle (Canvas, 0);
+            Return_Value :=
+              Handle (Colored_Check_Box_Type (Control.all).Brush);
          else
             On_Message (Window_Type (Window), message, wParam,
                         lParam, Return_Value);
@@ -136,6 +179,57 @@ package body GWindows.Colored_Control_Window is
       end if;
    end Color;
 
+   procedure Text_Color (Label : in out Colored_Label_Type;
+                         Color :        Color_Type) is
+   begin
+      if Color /= Label.Text then
+         Label.Text := Color;
+         Redraw (Label, True);
+      end if;
+   end Text_Color;
+
+   procedure Initialize (Edit : in out Colored_Edit_Box_Type) is
+   begin
+      Create_Solid_Brush (Edit.Brush, Edit.Color);
+      Initialize (Edit_Box_Type (Edit));
+   end Initialize;
+
+   procedure Color (Edit  : in out Colored_Edit_Box_Type;
+                    Color :        Color_Type) is
+   begin
+      if Color /= Edit.Color then
+         Edit.Color := Color;
+         Delete (Edit.Brush);
+         Create_Solid_Brush (Edit.Brush, Edit.Color);
+         Redraw (Edit, True);
+      end if;
+   end Color;
+
+   procedure Text_Color (Edit  : in out Colored_Edit_Box_Type;
+                         Color :        Color_Type) is
+   begin
+      if Color /= Edit.Text then
+         Edit.Text := Color;
+         Redraw (Edit, True);
+      end if;
+   end Text_Color;
+
+   procedure Default_Color (Edit : in out Colored_Edit_Box_Type) is
+   begin
+      if Read_Only (Edit) then
+         Color (Edit, System_Color (COLOR_BTNFACE));
+      else
+         Color (Edit, White);
+      end if;
+   end Default_Color;
+
+   procedure Read_Only (Edit  : in out Colored_Edit_Box_Type;
+                        State :        Boolean := True) is
+   begin
+      Read_Only (Edit_Box_Type (Edit), State);
+      Default_Color (Edit);
+   end Read_Only;
+
    procedure Initialize (Edit  : in out Colored_Multi_Line_Edit_Box_Type) is
    begin
       Create_Solid_Brush (Edit.Brush, Edit.Color);
@@ -153,6 +247,15 @@ package body GWindows.Colored_Control_Window is
       end if;
    end Color;
 
+   procedure Text_Color (Edit  : in out Colored_Multi_Line_Edit_Box_Type;
+                         Color :        Color_Type) is
+   begin
+      if Color /= Edit.Text then
+         Edit.Text := Color;
+         Redraw (Edit, True);
+      end if;
+   end Text_Color;
+
    procedure Initialize (Scroll_Bar : in out Colored_Scroll_Bar_Type) is
    begin
       Create_Solid_Brush (Scroll_Bar.Brush, Scroll_Bar.Color);
@@ -169,5 +272,164 @@ package body GWindows.Colored_Control_Window is
          Redraw (Scroll_Bar, True);
       end if;
    end Color;
+
+   procedure Initialize (Box : in out Colored_Combo_Box_Type) is
+   begin
+      Create_Solid_Brush (Box.Brush, Box.Color);
+      Initialize (Combo_Box_Type (Box));
+   end Initialize;
+
+   procedure Color (Box   : in out Colored_Combo_Box_Type;
+                    Color :        Color_Type) is
+   begin
+      if Color /= Box.Color then
+         Box.Color := Color;
+         Delete (Box.Brush);
+         Create_Solid_Brush (Box.Brush, Box.Color);
+         Redraw (Box, True);
+      end if;
+   end Color;
+
+   procedure Text_Color (Box   : in out Colored_Combo_Box_Type;
+                         Color :        Color_Type) is
+   begin
+      if Box.Text /= Color then
+         Box.Text := Color;
+         Redraw (Box, True);
+      end if;
+   end Text_Color;
+
+   procedure Default_Color (Box : in out Colored_Combo_Box_Type) is
+   begin
+      if Enabled (Box) then
+         Color (Box, White);
+      else
+         Color (Box, System_Color (COLOR_BTNFACE));
+      end if;
+   end Default_Color;
+
+   function Color (Box   : Colored_Combo_Box_Type)
+                   return Color_Type is
+   begin
+      return Box.Color;
+   end Color;
+
+   function Text_Color (Box   : Colored_Combo_Box_Type)
+                        return Color_Type is
+   begin
+      return Box.Text;
+   end Text_Color;
+
+   function Color_Brush (Box   : Colored_Combo_Box_Type)
+                         return GWindows.Types.Handle is
+   begin
+      return Handle (Box.Brush);
+   end Color_Brush;
+
+   procedure Initialize (Box : in out Colored_Drop_Down_Combo_Box_Type) is
+   begin
+      Create_Solid_Brush (Box.Brush, Box.Color);
+      Initialize (Drop_Down_Combo_Box_Type (Box));
+   end Initialize;
+
+   procedure Color (Box   : in out Colored_Drop_Down_Combo_Box_Type;
+                    Color :        Color_Type) is
+   begin
+      if Color /= Box.Color then
+         Box.Color := Color;
+         Delete (Box.Brush);
+         Create_Solid_Brush (Box.Brush, Box.Color);
+         Redraw (Box, True);
+      end if;
+   end Color;
+
+   procedure Text_Color (Box   : in out Colored_Drop_Down_Combo_Box_Type;
+                         Color :        Color_Type) is
+   begin
+      if Box.Text /= Color then
+         Box.Text := Color;
+         Redraw (Box, True);
+      end if;
+   end Text_Color;
+
+   procedure Default_Color (Box : in out Colored_Drop_Down_Combo_Box_Type) is
+   begin
+      if Enabled (Box) then
+         Color (Box, White);
+      else
+         Color (Box, System_Color (COLOR_BTNFACE));
+      end if;
+   end Default_Color;
+
+   function Color (Box   : Colored_Drop_Down_Combo_Box_Type)
+                   return Color_Type is
+   begin
+      return Box.Color;
+   end Color;
+
+   function Text_Color (Box   : Colored_Drop_Down_Combo_Box_Type)
+                        return Color_Type is
+   begin
+      return Box.Text;
+   end Text_Color;
+
+   function Color_Brush (Box   : Colored_Drop_Down_Combo_Box_Type)
+                         return GWindows.Types.Handle is
+   begin
+      return Handle (Box.Brush);
+   end Color_Brush;
+
+   procedure Initialize (Box : in out Colored_Check_Box_Type) is
+   begin
+      Box.Read_Only := False;
+      Create_Solid_Brush (Box.Brush, Box.Color);
+      Initialize (Check_Box_Type (Box));
+   end Initialize;
+
+   procedure Color (Box   : in out Colored_Check_Box_Type;
+                    Color :        Color_Type) is
+   begin
+      if Color /= Box.Color then
+         Box.Color := Color;
+         Delete (Box.Brush);
+         Create_Solid_Brush (Box.Brush, Box.Color);
+         Redraw (Box, True);
+      end if;
+   end Color;
+
+   procedure Default_Color (Box : in out Colored_Check_Box_Type) is
+   begin
+      Color (Box, System_Color (COLOR_BTNFACE));
+   end Default_Color;
+
+   function Color (Box : Colored_Check_Box_Type) return Color_Type is
+   begin
+      return Box.Color;
+   end Color;
+
+   function Color_Brush (Box : Colored_Check_Box_Type)
+                         return GWindows.Types.Handle is
+   begin
+      return Handle (Box.Brush);
+   end Color_Brush;
+
+   procedure Read_Only (Box   : in out Colored_Check_Box_Type;
+                        State :        Boolean := True) is
+   begin
+      Box.Read_Only := State;
+      Default_Color (Box);
+   end Read_Only;
+
+   procedure On_Command
+     (Box       : in out Colored_Check_Box_Type;
+      Code      : in     Integer;
+      ID        : in     Integer;
+      Control   : in     Pointer_To_Base_Window_Class) is
+      BN_CLICKED : constant := 0;
+   begin
+      if Code /= BN_CLICKED or else not Box.Read_Only then
+         On_Command (Check_Box_Type (Box), Code, ID, Control);
+      end if;
+   end On_Command;
 
 end GWindows.Colored_Control_Window;
--- gwindows/contrib/gwindows-colored_control_window.ads
+++ gwindows/contrib/gwindows-colored_control_window.ads
@@ -1,11 +1,15 @@
 --  Contribued package from Wiljan Derks - wiljan.derks@philips.com
 
-with GWindows.Windows;          use GWindows.Windows;
+with GWindows.Base;             use GWindows.Base;
+with GWindows.Buttons;          use GWindows.Buttons;
 with GWindows.Colors;           use GWindows.Colors;
+with GWindows.Combo_Boxes;      use GWindows.Combo_Boxes;
 with GWindows.Drawing_Objects;  use GWindows.Drawing_Objects;
 with GWindows.Edit_Boxes;       use GWindows.Edit_Boxes;
+with GWindows.Types;            use GWindows.Types;
 with GWindows.Scroll_Bars;      use GWindows.Scroll_Bars;
 with GWindows.Static_Controls;  use GWindows.Static_Controls;
+with GWindows.Windows;          use GWindows.Windows;
 with Interfaces.C;              use Interfaces.C;
 
 package GWindows.Colored_Control_Window is
@@ -26,25 +30,94 @@ package GWindows.Colored_Control_Window is
    procedure Initialize (Window : in out Colored_Control_Window_Type);
 
    type Colored_Label_Type is new Label_Type with private;
+   type Colored_Edit_Box_Type is new Edit_Box_Type with private;
    type Colored_Multi_Line_Edit_Box_Type is new
      Multi_Line_Edit_Box_Type with private;
    type Colored_Scroll_Bar_Type is new
      GWindows.Scroll_Bars.Scroll_Bar_Type with private;
+   type Colored_Combo_Box_Type is new
+     GWindows.Combo_Boxes.Combo_Box_Type with private;
+   type Colored_Drop_Down_Combo_Box_Type is new
+     GWindows.Combo_Boxes.Drop_Down_Combo_Box_Type with private;
+   type Colored_Check_Box_Type is new
+     GWindows.Buttons.Check_Box_Type with private;
 
    procedure Initialize (Label  : in out Colored_Label_Type);
 
    procedure Color (Label : in out Colored_Label_Type;
-                    Color : Color_Type);
+                    Color :        Color_Type);
+   procedure Text_Color (Label : in out Colored_Label_Type;
+                         Color :        Color_Type);
+
+   procedure Initialize (Edit  : in out Colored_Edit_Box_Type);
+
+   procedure Color (Edit  : in out Colored_Edit_Box_Type;
+                    Color :        Color_Type);
+   procedure Text_Color (Edit  : in out Colored_Edit_Box_Type;
+                         Color :        Color_Type);
+
+   procedure Default_Color (Edit : in out Colored_Edit_Box_Type);
+
+   procedure Read_Only (Edit  : in out Colored_Edit_Box_Type;
+                        State :        Boolean := True);
 
    procedure Initialize (Edit : in out Colored_Multi_Line_Edit_Box_Type);
 
-   procedure Color (Edit : in out Colored_Multi_Line_Edit_Box_Type;
-                    Color : Color_Type);
+   procedure Color (Edit  : in out Colored_Multi_Line_Edit_Box_Type;
+                    Color :        Color_Type);
+   procedure Text_Color (Edit  : in out Colored_Multi_Line_Edit_Box_Type;
+                         Color :        Color_Type);
 
    procedure Initialize (Scroll_Bar : in out Colored_Scroll_Bar_Type);
 
    procedure Color (Scroll_Bar : in out Colored_Scroll_Bar_Type;
-                    Color : Color_Type);
+                    Color      :        Color_Type);
+
+   procedure Initialize (Box : in out Colored_Combo_Box_Type);
+
+   procedure Color (Box   : in out Colored_Combo_Box_Type;
+                    Color :        Color_Type);
+   procedure Text_Color (Box   : in out Colored_Combo_Box_Type;
+                         Color :        Color_Type);
+   function Color (Box   : Colored_Combo_Box_Type)
+                   return Color_Type;
+   function Text_Color (Box   : Colored_Combo_Box_Type)
+                        return Color_Type;
+   function Color_Brush (Box   : Colored_Combo_Box_Type)
+                         return GWindows.Types.Handle;
+
+   procedure Default_Color (Box : in out Colored_Combo_Box_Type);
+
+   procedure Initialize (Box : in out Colored_Drop_Down_Combo_Box_Type);
+
+   procedure Color (Box   : in out Colored_Drop_Down_Combo_Box_Type;
+                    Color :        Color_Type);
+   procedure Text_Color (Box   : in out Colored_Drop_Down_Combo_Box_Type;
+                         Color :        Color_Type);
+   function Color (Box   : Colored_Drop_Down_Combo_Box_Type)
+                   return Color_Type;
+   function Text_Color (Box   : Colored_Drop_Down_Combo_Box_Type)
+                        return Color_Type;
+   function Color_Brush (Box   : Colored_Drop_Down_Combo_Box_Type)
+                         return GWindows.Types.Handle;
+
+   procedure Default_Color (Box : in out Colored_Drop_Down_Combo_Box_Type);
+
+   procedure Initialize (Box : in out Colored_Check_Box_Type);
+
+   procedure Color (Box   : in out Colored_Check_Box_Type;
+                    Color :        Color_Type);
+   function Color (Box   : Colored_Check_Box_Type) return Color_Type;
+   function Color_Brush (Box   : Colored_Check_Box_Type)
+                         return GWindows.Types.Handle;
+
+   procedure Default_Color (Box : in out Colored_Check_Box_Type);
+
+   --  When a check box is disabled to prevent a user from modifying it,
+   --  the tooltip does not work anymore. Therefor this Read_Only
+   --  procedure has been implemented.
+   procedure Read_Only (Box   : in out Colored_Check_Box_Type;
+                        State :        Boolean := True);
 
 private
 
@@ -60,12 +133,21 @@ private
    type Colored_Label_Type is new Label_Type with
       record
          Color : Color_Type := System_Color (COLOR_BTNFACE);
+         Text  : Color_Type := Black;
+         Brush : Brush_Type;
+      end record;
+
+   type Colored_Edit_Box_Type is new Edit_Box_Type with
+      record
+         Color : Color_Type := White;
+         Text  : Color_Type := Black;
          Brush : Brush_Type;
       end record;
 
    type Colored_Multi_Line_Edit_Box_Type is new Multi_Line_Edit_Box_Type with
       record
          Color : Color_Type := System_Color (COLOR_BTNFACE);
+         Text  : Color_Type := Black;
          Brush : Brush_Type;
       end record;
 
@@ -75,5 +157,34 @@ private
          Color : Color_Type := System_Color (COLOR_SCROLLBAR);
          Brush : Brush_Type;
       end record;
+
+   type Colored_Combo_Box_Type is new
+     GWindows.Combo_Boxes.Combo_Box_Type with
+      record
+         Color : Color_Type := System_Color (COLOR_SCROLLBAR);
+         Text  : Color_Type := Black;
+         Brush : Brush_Type;
+      end record;
+
+   type Colored_Drop_Down_Combo_Box_Type is new
+     GWindows.Combo_Boxes.Drop_Down_Combo_Box_Type with
+      record
+         Color : Color_Type := System_Color (COLOR_SCROLLBAR);
+         Text  : Color_Type := Black;
+         Brush : Brush_Type;
+      end record;
+
+   type Colored_Check_Box_Type is new GWindows.Buttons.Check_Box_Type with
+      record
+         Color     : Color_Type := System_Color (COLOR_BTNFACE);
+         Brush     : Brush_Type;
+         Read_Only : Boolean := False;
+      end record;
+
+   procedure On_Command
+     (Box     : in out Colored_Check_Box_Type;
+      Code    : in     Integer;
+      ID      : in     Integer;
+      Control : in     Pointer_To_Base_Window_Class);
 
 end GWindows.Colored_Control_Window;
