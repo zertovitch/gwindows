02786c790e2b3944e8073cae3db8f1711dbfeecb
gdemont <gdemont@2f326fbb-0abf-4209-8265-e07f4167bd40>
2010-02-18 19:16:45 +0000

    GWindows: added GWindows.Buttons.Graphic (technically, buttons with BS_BITMAP or BS_ICON)

---
+++ gwindows/framework/gwindows-buttons-graphic.adb
@@ -0,0 +1,90 @@
+package body GWindows.Buttons.Graphic is
+
+   use Interfaces.C;
+
+   GWL_STYLE   : constant:= -16;
+
+   procedure SetWindowLong(hwnd    : Interfaces.C.long;
+                           nIndex  : Interfaces.C.int := GWL_STYLE;
+                           newLong : Interfaces.C.unsigned);
+   pragma Import (StdCall, SetWindowLong,
+                    "SetWindowLong" & Character_Mode_Identifier);
+   function GetWindowLong(hwnd   : Interfaces.C.long;
+                          nIndex : Interfaces.C.int := GWL_STYLE)
+                         return Interfaces.C.unsigned;
+   pragma Import (StdCall, GetWindowLong,
+                    "GetWindowLong" & Character_Mode_Identifier);
+
+   BS_ICON     : constant := 16#40#;
+   BS_BITMAP   : constant := 16#80#;
+   BS_FLAT     : constant := 16#8000#;
+   BM_SETIMAGE : constant := 247;
+   IMAGE_BITMAP: constant := 0;
+   IMAGE_ICON  : constant := 1;
+   -- WS_BORDER   : constant := 8388608;
+
+   ---------------
+   -- On_Create --
+   ---------------
+
+   procedure On_Create (Window : in out Icon_Button_Type) is
+   begin
+     SetWindowLong (Handle(Window),
+                    newLong => GetWindowLong (Handle (Window)) or BS_ICON or BS_FLAT);
+   end On_Create;
+
+   --------------
+   -- Set_Icon --
+   --------------
+
+   procedure Set_Icon (
+      Window : in out Icon_Button_Type;
+      Icon   : GWindows.Drawing_Objects.Icon_Type
+   )
+   is
+      use GWindows.Drawing_Objects;
+      -- http://msdn.microsoft.com/en-us/library/bb761822(VS.85).aspx
+      procedure Sendmessage (Hwnd   : Interfaces.C.Long := Handle (Window);
+                             Umsg   : Interfaces.C.Int  := BM_SETIMAGE;
+                             Wparam : Integer           := IMAGE_ICON;
+                             Lparam : Interfaces.C.long := Handle(Icon)
+                            );
+      pragma Import (Stdcall, Sendmessage,
+                     "SendMessage" & Character_Mode_Identifier);
+   begin
+      Sendmessage;
+   end Set_Icon;
+
+   ---------------
+   -- On_Create --
+   ---------------
+
+   procedure On_Create (Window : in out Bitmap_Button_Type) is
+   begin
+     SetWindowLong (Handle(Window),
+                    newLong => GetWindowLong (Handle (Window)) or BS_Bitmap or BS_FLAT);
+   end On_Create;
+
+   --------------
+   -- Set_Bitmap --
+   --------------
+
+   procedure Set_Bitmap (
+      Window : in out Bitmap_Button_Type;
+      Bitmap : GWindows.Drawing_Objects.Bitmap_Type
+   )
+   is
+      use GWindows.Drawing_Objects;
+      -- http://msdn.microsoft.com/en-us/library/bb761822(VS.85).aspx
+      procedure Sendmessage (Hwnd   : Interfaces.C.Long := Handle (Window);
+                             Umsg   : Interfaces.C.Int  := BM_SETIMAGE;
+                             Wparam : Integer           := IMAGE_BITMAP;
+                             Lparam : Interfaces.C.long := Handle(Bitmap)
+                            );
+      pragma Import (Stdcall, Sendmessage,
+                     "SendMessage" & Character_Mode_Identifier);
+   begin
+      Sendmessage;
+   end Set_Bitmap;
+
+end GWindows.Buttons.Graphic;
---
+++ gwindows/framework/gwindows-buttons-graphic.ads
@@ -0,0 +1,34 @@
+-- Inspired by Gwindows.Buttons.Ex_buttons, but with less features
+-- especially: no tasking (and related stack issues in GNAT)
+
+with GWindows.Drawing_Objects;
+
+package GWindows.Buttons.Graphic is
+
+   ----------------------
+   -- Icon_Button_Type --
+   ----------------------
+
+   type Icon_Button_Type is new Button_Type with null record;
+
+   procedure On_Create (Window : in out Icon_Button_Type);
+
+   procedure Set_Icon (
+      Window : in out Icon_Button_Type;
+      Icon   : GWindows.Drawing_Objects.Icon_Type
+   );
+
+   ------------------------
+   -- Bitmap_Button_Type --
+   ------------------------
+
+   type Bitmap_Button_Type is new Button_Type with null record;
+
+   procedure On_Create (Window : in out Bitmap_Button_Type);
+
+   procedure Set_Bitmap (
+      Window : in out Bitmap_Button_Type;
+      Bitmap : GWindows.Drawing_Objects.Bitmap_Type
+   );
+
+end GWindows.Buttons.Graphic;
