9ffafc6fececebbb9c6b6cb01af66874caeea686
gdemont <gdemont@2f326fbb-0abf-4209-8265-e07f4167bd40>
2012-01-29 17:13:10 +0000

    GWindows Installer: release build in 'Small' mode

--- gwindows/installer/build.cmd
+++ gwindows/installer/build.cmd
@@ -1,5 +1,5 @@
 if "%1"=="-gen" ..\..\gwenerator\rc2gw GW_Install.rc
 if "%1"=="-gen" shift
 
-gnatmake -P GW_Install.gpr
+gnatmake -P GW_Install.gpr -XBuild_Mode=Small
 copy /B gw_install.exe + gwin.zip "GWindows Setup.exe"
