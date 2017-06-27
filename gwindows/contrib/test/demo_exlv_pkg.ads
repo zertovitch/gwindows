63e18787f693e36b9054e6a6328a1592f35f9030
gdemont <gdemont@2f326fbb-0abf-4209-8265-e07f4167bd40>
2017-06-27 19:33:39 +0000

    Style/warning gardening (build with GNAT GPL 2017)

--- gwindows/contrib/test/demo_exlv_pkg.ads
+++ gwindows/contrib/test/demo_exlv_pkg.ads
@@ -14,7 +14,7 @@ package Demo_exlv_Pkg is
    type Payload_Data_Access is access all Payload_Data_Type;
 
    -- instantiation with user defined payload type
-   package My_List_View_Pkg is new Gwindows.Common_Controls.Ex_List_View(Payload_Data_type);
+   package My_List_View_Pkg is new Gwindows.Common_Controls.Ex_List_View(Payload_Data_Type);
    type My_List_View_Type is new My_List_View_Pkg.Ex_List_View_Control_Type with null record;
 
    procedure Free_payload is new Ada.Unchecked_Deallocation(Payload_Data_Type,
