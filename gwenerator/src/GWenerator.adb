8026545221412525f6345baf06f967e07c6c5997
gdemont <gdemont@2f326fbb-0abf-4209-8265-e07f4167bd40>
2018-09-08 13:21:18 +0000

    GWenerator rc2gw can't inadvertently be built without console output (-mwindows)

--- gwenerator/src/GWenerator.adb
+++ gwenerator/src/GWenerator.adb
@@ -2,7 +2,8 @@ with GWindows.Application;
 with GWen_Windows;
 
 procedure GWenerator is
-  --  pragma Linker_Options ("-mwindows");
+  --  Comment out the following pragma for eventual text output (debugging)
+  pragma Linker_Options ("-mwindows");
   main_window: GWen_Windows.GWen_Window_Type;
 begin
   main_window.Create("GWenerator - starting...");
