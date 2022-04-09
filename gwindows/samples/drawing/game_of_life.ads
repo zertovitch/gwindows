72017a7a3c1829ae67496103bfb60cbda6f0018d
zertovitch <gdemont@hotmail.com>
2022-04-09 12:16:54 +0200

    (samples) Game of Life: added instructions

--- gwindows/samples/drawing/game_of_life.ads
+++ gwindows/samples/drawing/game_of_life.ads
@@ -1,13 +1,8 @@
-package Game_of_Life is  --  Abstract Game of Life, could be moved out of this demo.
-   type State is (Dead, Alive);
-   type Map_Type is array (Positive range <>, Positive range <>) of State;
-   --
-   procedure Move (
-      current_map : in     Map_Type;
-      new_map     :    out Map_Type
-   );
+--  Abstract Game of Life (pure Ada, unrelated to any graphics system).
+  
+package Game_of_Life is
 
-   procedure Clear (Map : out Map_Type);
+   type State is (Dead, Alive);
 
    type Figure is (
      Point,
@@ -25,6 +20,15 @@ package Game_of_Life is  --  Abstract Game of Life, could be moved out of this d
      HWSS
    );
 
+   type Map_Type is array (Positive range <>, Positive range <>) of State;
+
    procedure Add_Figure (Map : out Map_Type; xc, yc : Integer; f : Figure; s : State);
+
+   procedure Clear (Map : out Map_Type);
+
+   procedure Move (
+      current_map : in     Map_Type;
+      new_map     :    out Map_Type
+   );
 
 end Game_of_Life;
