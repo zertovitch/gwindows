26bb875d50975f8d90213d62d5a29e544e3668e3
unknown <gdemont@hotmail.com>
2025-03-15 18:04:12 +0100

    License change GMGPL -> MIT (4/n: GWindows framework)

--- gwindows/framework/coding/gwindows_ansi.ads
+++ gwindows/framework/coding/gwindows_ansi.ads
@@ -9,23 +9,26 @@
 --                                                                          --
 --                 Copyright (C) 1999 - 2022 David Botton                   --
 --                                                                          --
--- This is free software;  you can  redistribute it  and/or modify it under --
--- terms of the  GNU General Public License as published  by the Free Soft- --
--- ware  Foundation;  either version 2,  or (at your option) any later ver- --
--- sion. It is distributed in the hope that it will be useful,  but WITHOUT --
--- ANY WARRANTY;  without  even the  implied warranty of MERCHANTABILITY or --
--- FITNESS FOR A PARTICULAR PURPOSE.    See the GNU General  Public License --
--- for  more details.  You should have  received  a copy of the GNU General --
--- Public License  distributed with this;  see file COPYING.  If not, write --
--- to  the Free Software Foundation,  59 Temple Place - Suite 330,  Boston, --
--- MA 02111-1307, USA.                                                      --
+-- MIT License                                                              --
 --                                                                          --
--- As a special exception,  if other files  instantiate  generics from this --
--- unit, or you link  this unit with other files  to produce an executable, --
--- this  unit  does not  by itself cause  the resulting  executable  to  be --
--- covered  by the  GNU  General  Public  License.  This exception does not --
--- however invalidate  any other reasons why  the executable file  might be --
--- covered by the  GNU Public License.                                      --
+-- Permission is hereby granted, free of charge, to any person obtaining    --
+-- a copy of this software and associated documentation files (the          --
+-- "Software"), to deal in the Software without restriction, including      --
+-- without limitation the rights to use, copy, modify, merge, publish,      --
+-- distribute, sublicense, and/or sell copies of the Software, and to       --
+-- permit persons to whom the Software is furnished to do so, subject to    --
+-- the following conditions:                                                --
+--                                                                          --
+-- The above copyright notice and this permission notice shall be included  --
+-- in all copies or substantial portions of the Software.                   --
+--                                                                          --
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,          --
+-- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF       --
+-- MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.   --
+-- IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY     --
+-- CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,     --
+-- TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE        --
+-- SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                   --
 --                                                                          --
 -- More information about GWindows and the latest current release can       --
 -- be located on the web at one of the following places:                    --
